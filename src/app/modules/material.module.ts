b120f710d1e7748ab968b0f51a3d03eb24fa9944
Nick Landkamer <nicklandkamer@gmail.com>
2020-07-18 10:22:17 -0500

    styled sitenav

--- src/app/modules/material.module.ts
+++ src/app/modules/material.module.ts
@@ -1,8 +1,19 @@
 import { NgModule } from '@angular/core';
 import { FlexLayoutModule } from '@angular/flex-layout';
+import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
+import { MatIconModule } from '@angular/material/icon';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
-const modules = [FlexLayoutModule, MatCardModule];
+const modules = [
+  FlexLayoutModule,
+  MatButtonModule,
+  MatCardModule,
+  MatMenuModule,
+  MatIconModule,
+  MatToolbarModule,
+];
 
 @NgModule({
   declarations: [],
